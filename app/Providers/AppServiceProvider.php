ecdab8f978d14d5c4dff46ec3046cd3dcc5bf623
vlachowskiii-hub <vlachowskiii@gmail.com>
2026-08-22 10:43:02 +0700

    p

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL; // <-- Tambahkan baris ini
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        // Tambahkan blok ini untuk memaksa HTTPS di production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
